1d40e25b6a3ca3bf7c278802a3778897e597b7ed
Heiko Hylla <heikohylla@lmmu-fq592n60nm.united.domain>
2025-05-20 13:32:05 +0200

    Erster Commit

---
+++ app/src/main/java/de/test/weatherapp/ui/theme/FrontView.kt
@@ -0,0 +1,23 @@
+package de.test.weatherapp.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun Buttons() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Yellow)
+    ) {
+        Button(onClick = { }) {
+            Text("test")
+        }
+    }
+}
